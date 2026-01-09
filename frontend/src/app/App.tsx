be769a8c7742ebb463fc97699d4384bafd58ef30
anton-oxota <unearthlylight@gmail.com>
2026-01-09 12:52:29 +0200

    router init

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,9 @@
+import { RouterProvider } from "react-router/dom";
+
+import router from "./routes";
+
 function App() {
-    return <h1>Hello world</h1>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/app/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        index: true,
+        element: <h1>HomePage</h1>,
+    },
+]);
+
+export default router;
