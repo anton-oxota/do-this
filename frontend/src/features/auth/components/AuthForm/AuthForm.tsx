375d84225f11d206dc8b099c8bee2904b699daf1
anton-oxota <unearthlylight@gmail.com>
2026-01-16 14:20:24 +0200

    feat(auth): add validation logit to AuthForm - add useFrom hook with custom validation logic - add input error style to FormInput

--- frontend/src/features/auth/components/AuthForm/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm/AuthForm.tsx
@@ -4,12 +4,15 @@ import { Link } from "react-router";
 
 import FormInput from "../../ui/FormInput/FormInput";
 import type { AuthModes } from "../../types/auth";
+import useForm from "../../../../shared/hooks/useForm";
 
 type AuthFormProps = {
     mode: AuthModes;
 };
 
 function AuthForm({ mode }: AuthFormProps) {
+    const { errors, touched, values, register, reset, onSubmit } = useForm();
+
     const isRegistration = mode === "register";
 
     const linkTo = isRegistration ? "/auth/login" : "/auth/register";
@@ -19,24 +22,67 @@ function AuthForm({ mode }: AuthFormProps) {
 
     return (
         <>
-            <form className={css.form}>
+            <form
+                className={css.form}
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    onSubmit((data) => {
+                        console.log(data);
+                    });
+                }}
+            >
                 <h1>&bull; DoThis &bull;</h1>
 
                 <div className={css.formInputs}>
                     {isRegistration && (
-                        <FormInput labelText="Name" type="text" />
+                        <FormInput
+                            labelText="Name"
+                            type="text"
+                            isTouched={touched["name"]}
+                            errorMessage={errors["name"]}
+                            {...register("name", {
+                                pattern: /^[A-Za-z\s'-]{2,50}$/,
+                                errorMessage:
+                                    "Name must be at least 2 character",
+                            })}
+                        />
                     )}
-                    <FormInput labelText="Email" type="email" />
-                    <FormInput labelText="Password" type="password" />
+                    <FormInput
+                        labelText="Email"
+                        type="email"
+                        isTouched={touched["email"]}
+                        errorMessage={errors["email"]}
+                        {...register("email", {
+                            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                            errorMessage: "Enter valid email",
+                        })}
+                    />
+                    <FormInput
+                        labelText="Password"
+                        type="password"
+                        isTouched={touched["password"]}
+                        errorMessage={errors["password"]}
+                        {...register("password", {
+                            pattern: /^.{6,}$/,
+                            errorMessage:
+                                "Passowrd must be at least 6 character",
+                        })}
+                    />
                     {isRegistration && (
                         <FormInput
                             labelText="Confirm Password"
                             type="password"
+                            isTouched={touched["confirmPassowrd"]}
+                            errorMessage={errors["confirmPassowrd"]}
+                            {...register("confirmPassowrd", {
+                                pattern: values["password"],
+                                errorMessage: "Passwords must be equal",
+                            })}
                         />
                     )}
                 </div>
 
-                <Link className={css.formLink} to={linkTo}>
+                <Link className={css.formLink} to={linkTo} onClick={reset}>
                     {linkText}
                 </Link>
                 <button type="submit">
--- frontend/src/features/auth/ui/FormInput/FormInput.tsx
+++ frontend/src/features/auth/ui/FormInput/FormInput.tsx
@@ -2,13 +2,28 @@ import css from "./FormInput.module.css";
 
 type FormInputType = {
     labelText: string;
+    isTouched: boolean;
+    errorMessage: string | null;
 } & React.ComponentProps<"input">;
 
-function FormInput({ labelText, id, ...inputProps }: FormInputType) {
+function FormInput({
+    labelText,
+    id,
+    isTouched,
+    errorMessage,
+    ...inputProps
+}: FormInputType) {
+    const isErrorVisible = isTouched && errorMessage;
+    let inputWrapperClasses = css.inputWrapper;
+    if (isErrorVisible) inputWrapperClasses += ` ${css.error}`;
+
     return (
-        <div className={css.inputWrapper}>
+        <div className={inputWrapperClasses}>
             <label htmlFor={id}>{labelText}</label>
             <input className={css.input} {...inputProps} />
+            {isTouched && errorMessage && (
+                <span style={{ color: "red" }}>{errorMessage}</span>
+            )}
         </div>
     );
 }
